4df8a17509e539d3df1221c0c0e9685bd121d7e3
Gabriel Ximenes Mitozo Noronha <gabriel.noronha@grupotapajos.com.br>
2025-01-08 15:40:18 -0400

    feat:add docker

--- next.config.js
+++ next.config.js
@@ -1,11 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        buffer: require.resolve('buffer/'),
+      };
+    }
+    return config;
+  },
 
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  server: {
+    https: true,
+    http2: true
+  }
 };
 
 module.exports = nextConfig;

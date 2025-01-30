beb004379e6f4947a6bbd1b657b2e3b145066269
Gabriel Ximenes Mitozo Noronha <gabriel.noronha@grupotapajos.com.br>
2025-01-30 09:05:44 -0400

    feat:refact api link

--- app/service/api.ts
+++ app/service/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.1.100:3001/api/', 
+    baseURL: 'http://10.2.10.17:3001/api/', 
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'

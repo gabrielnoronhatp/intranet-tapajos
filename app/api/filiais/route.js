c68542fa34b96a5f4c8266dab0768583ddbca263
Gabriel Ximenes Mitozo Noronha <gabriel.noronha@grupotapajos.com.br>
2025-03-10 10:27:33 -0400

    feat:ident and add edit campaning

--- app/api/filiais/route.js
+++ app/api/filiais/route.js
@@ -8,6 +8,7 @@ const pool = new Pool({
     database: process.env.PG_DATABASE,
     port: 5432,
 });
+export const dynamic = 'force-dynamic'; // Adicione esta linha
 
 export async function GET(request) {
     try {
--- next.config.js
+++ next.config.js
@@ -1,9 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    compress: true,
     eslint: {
         ignoreDuringBuilds: true,
     },
-    images: { unoptimized: true },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    experimental: {
+        serverComponentsExternalPackages: ['sequelize'],
+    },
+    // Nova configuração para resolver o erro de minificação
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.optimization.minimizer.forEach((plugin) => {
+                if (plugin.constructor.name === 'TerserPlugin') {
+                    plugin.options.terserOptions.keep_classnames = true;
+                    plugin.options.terserOptions.keep_fnames = true;
+                }
+            });
+        }
+        return config;
+    },
 };
 
 module.exports = nextConfig;
